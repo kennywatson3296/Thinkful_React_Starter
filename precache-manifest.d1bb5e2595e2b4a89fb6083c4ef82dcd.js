2fac2428b252e7ac2660a6c55fd1f43d42d93f4d
Kenny Watson <kennywatson3296@gmail.com>
2023-07-22 03:37:03 -0400

    Updates

---
+++ precache-manifest.d1bb5e2595e2b4a89fb6083c4ef82dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede28f1da5867ad4bde2b51144ffb4b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfa40fff2a94989edf9e",
+    "url": "/static/css/main.097536d1.chunk.css"
+  },
+  {
+    "revision": "ca7bb87eccffc7c67f94",
+    "url": "/static/js/2.d197d08f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d197d08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa40fff2a94989edf9e",
+    "url": "/static/js/main.74fe58e3.chunk.js"
+  },
+  {
+    "revision": "c8e5fd3b141acc9e29e7",
+    "url": "/static/js/runtime-main.e51d1157.js"
+  }
+]);
